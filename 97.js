3a6441a8348c173c20418fddcee932f63ed658a7
Prince Changani <princechangani56385638@gmail.com>
2024-01-06 23:40:01 +0530

    function execution context
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- 97.js
+++ 97.js
@@ -1,17 +1,33 @@
 // function execution context
 
-let foo = "foo";
-console.log(foo);
+// let foo = "foo";
+// console.log(foo);
 
-function getFullName(firstName, lasatName)
+// function getFullName(firstName, lasatName)
+// {
+//     console.log(arguments);
+//     console.log(arguments.length);
+//     let myvar = "var inside function";
+//     console.log(myvar);
+//     const fullName = firstName + " " + lasatName;
+//     return fullName;
+// }
+
+// const personName = getFullName("Prince", "changani");
+// console.log(personName);
+
+// challenge 
+const printForcast = function(arr)
 {
-    console.log(arguments);
-    console.log(arguments.length);
-    let myvar = "var inside function";
-    console.log(myvar);
-    const fullName = firstName + " " + lasatName;
-    return fullName;
+    // const celsius = [];
+    let str = ''; 
+    for(let i = 0; i< arr.length; i++)
+    {
+        // celsius.push(arr[i]+270);
+        str = str + `${arr[i]}°C in ${i+1} day... `;
+    }
+    console.log("..."+str);
 }
 
-const personName = getFullName("Prince", "changani");
-console.log(personName);
+const array = [17, 21, 23];
+printForcast(array);
